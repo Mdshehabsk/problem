b6e6ab1570ac6a24f421cfc82df0a59cf98bd20d
mdshehabsk <mdshehabsk@gmail.com>
2022-03-27 23:07:27 +0600

    env file include

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connect = async ()=>{
-     mongoose.connect('mongodb://localhost:27017/crud')
+     mongoose.connect(process.env.DBURL)
      .then(data=>console.log(`db connect successfull`))
      .catch(err=>console.log(err))
 }
